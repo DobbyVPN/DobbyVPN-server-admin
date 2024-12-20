d652afb7eac78ec0fffc362e7d1e3cc89545fd3d
xaeliudzyh <gtsagolov8@gmail.com>
2024-12-20 19:36:36 +0300

    fixed bugs

--- ui/rename_key_view.py
+++ ui/rename_key_view.py
@@ -54,6 +54,7 @@ class RenameKeyView(BaseScreen):
                 self.refresh_keys()
                 self.load_devices()
                 #self.app.push_screen(MessageView("Ready", "The key has been renamed."))
+                self.app.pop_screen()
             else:
                 self.app.push_screen(MessageView("Error", "The new name cannot be empty."))
         else:
